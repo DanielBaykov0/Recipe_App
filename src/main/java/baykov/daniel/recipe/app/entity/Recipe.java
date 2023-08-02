3ff1b4b81d8e76798d9aa16e2c16a9eb0d04ddc8
Daniel Baykov <danibaikov@gmail.com>
2023-08-02 12:29:52 +0300

    adding recipe entity

---
+++ src/main/java/baykov/daniel/recipe/app/entity/Recipe.java
@@ -0,0 +1,31 @@
+package baykov.daniel.recipe.app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "recipe")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private List<String> ingredients;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int rating;
+}
