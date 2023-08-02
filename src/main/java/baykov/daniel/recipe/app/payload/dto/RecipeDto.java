f60d51693ff21b5e7b495a443951b09fd7ebe0c9
Daniel Baykov <danibaikov@gmail.com>
2023-08-02 13:06:23 +0300

    modifying dependencies

--- src/main/java/baykov/daniel/recipe/app/payload/dto/RecipeDto.java
+++ src/main/java/baykov/daniel/recipe/app/payload/dto/RecipeDto.java
@@ -3,8 +3,10 @@ package baykov.daniel.recipe.app.payload.dto;
 import java.util.List;
 
 public record RecipeDto(
-    List<String> ingredients,
-    String description,
-    Integer rating
+
+        Long id,
+        List<String> ingredients,
+        String description,
+        Integer rating
 ) {
 }
