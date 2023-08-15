509fb091b8040507142b18906165dbf1f7eb59f1
Daniel Baykov <danibaikov@gmail.com>
2023-08-15 15:28:49 +0300

    adding recipe repository

--- src/main/java/baykov/daniel/recipe/app/repository/RecipeRepository.java
+++ src/main/java/baykov/daniel/recipe/app/repository/RecipeRepository.java
@@ -1,6 +1,6 @@
 package baykov.daniel.recipe.app.repository;
 
-import baykov.daniel.recipe.app.entity.Recipe;
+import baykov.daniel.recipe.app.model.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
