d164bfd517f1986f4b3b11c74bb94920b2f71714
Daniel Baykov <danibaikov@gmail.com>
2023-08-16 20:45:36 +0300

    updating ingredient repository

--- src/main/java/baykov/daniel/recipe/app/repository/IngredientRepository.java
+++ src/main/java/baykov/daniel/recipe/app/repository/IngredientRepository.java
@@ -3,5 +3,9 @@ package baykov.daniel.recipe.app.repository;
 import baykov.daniel.recipe.app.model.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+
+    Optional<Ingredient> findByName(String name);
 }
